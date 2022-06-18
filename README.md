9440516325496601215524e4ccf3d9dee5c5e1f4
Brian Park <brian.parkjy@gmail.com>
2022-06-18 15:17:35 -0700

    [STYLE] add comments and update README.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # user-login-feature
 Basic CLI version of user authentication (register &amp; login)
+
+### What I built
+---
+- Store user credentials in a database (sqlite)
+- Use hashing to store passwords
+- Allow user to register
+- Allow user to login
+
+### Things to expand on
+---
+- [x] Store user credentials in a database (sqlite)
+- [x] Use hashing to store passwords
+- [x] Allow user to register
+- [x] Allow user to login
+- [ ] prevent SQL-injection
+- [ ] Once user is verified, allow user to update password or delete account
+- [ ] Create password requirements (min length, use of numbers, use of special characters)
+- [ ] Implement some form of two-factor authentication
+- [ ] Implement login component in a web application
--- feature_user_login.py
+++ feature_user_login.py
@@ -2,36 +2,68 @@ import hashlib
 import sqlite3
 from getpass import getpass
 
-# import json
-# import os
-
 conn = sqlite3.connect('./users.db')
 c = conn.cursor()
 
 def setup_db():
+    '''
+    creates table "users" in .db file
+
+    @param none
+    @returns none
+    '''
     q = '''CREATE TABLE IF NOT EXISTS users (
         username TEXT PRIMARY KEY,
-        password TEXT
-    );'''
+        password TEXT);
+        '''
     c.execute(q)
 
 def drop_table():
     c.execute('''DROP TABLE users;''')
 
 def user_count():
+    '''
+    Sends query to db to check for pre-existing data
+
+    @param none
+    @returns {int} returns the number of rows in table "users"
+    '''
     q = '''SELECT COUNT(*) FROM users;'''
     c.execute(q)
     return c.fetchone()[0]
 
 def check_credentials(username, password):
-    password = get_hash(password)
-    c.execute('''SELECT * FROM users WHERE username = ? AND password = ?;''', (username, password))
+    '''
+    Given a username & password input, applies SHA256 hash to password 
+    and sends a query to check if the user exists.
+
+    @param {string} username input to check against db
+    @param {string} password input to check against db
+    @returns {boolean} returns True if user exists + password matches, else False
+    '''
+    hash_pw = get_hash(password)
+    c.execute('''SELECT * FROM users WHERE username = ? AND password = ?;''', \
+             (username, hash_pw))
     return c.fetchone() is not None
 
-def get_hash(input): 
+def get_hash(input):
+    '''
+    Given a string input, calculates the SHA256 hash of the input.
+
+    @param {string} input to be hashed
+    @returns {string} returns the result of the SHA256 algorithm in hexadecimal format
+    ''' 
     return hashlib.sha256(input.encode('utf-8')).hexdigest()
 
 def add_user(username, password):
+    '''
+    Given a username and password, applies SHA256 hash to password and
+    sends a query to add the user to the db.
+
+    @param {string} username input to insert into db
+    @param {string} password input to insert into db
+    @returns {boolean} returns True if user is added, else False (username is taken)
+    '''
     password = get_hash(password)
 
     try:
@@ -47,18 +79,20 @@ def main():
     row_num = user_count()
     print("There are currently %d number of rows in the table." % row_num)
 
-    create_table = input("Would you like to delete the current table and create a new one? (y/n): ").lower().strip()
+    create_table = input("Would you like to delete the current table" \
+                        "and create a new one? (y/n): ").lower().strip()
     if create_table in ['y', 'yes']:
         drop_table()
         setup_db()
 
     while True:
-        response = input("Please choose the action you would like to perform.\nType \"register\" | \"login\" | \"quit\" ").lower().strip()
+        response = input("Please choose the action you would like to perform. \
+                        \nType \"register\" or \"login\" or \"quit\" ").lower().strip()
         if response == 'register':
             while True:
-                username = input("Please enter the username you would like to create: ").lower()
+                username = input("Please enter the username: ").lower()
                 while True:
-                    pw = getpass("Please enter the password you would like to use: ")
+                    pw = getpass("Please enter the password: ")
                     retype_pw = getpass("Please confirm your password: ")
 
                     if pw == retype_pw:
@@ -70,7 +104,7 @@ def main():
                     print(f"User has been created! Username: {username}")
                     break
                 else:
-                    print("Username already taken, please enter a different username.")
+                    print("Username already taken. Please try again.")
             
         elif response == 'login':
             while True:
